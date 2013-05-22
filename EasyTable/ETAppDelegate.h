08903911aa56048762df26c320ea1c32e6263b78
Frank Mathy <cb3mank@MacBook-NB03.local>
2013-05-22 16:35:06 +0200

    Initial Commit

---
+++ EasyTable/ETAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ETAppDelegate.h
+//  EasyTable
+//
+//  Created by Frank Mathy on 22.05.13.
+//  Copyright (c) 2013 Frank Mathy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ETAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ EasyTable/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  EasyTable
+//
+//  Created by Frank Mathy on 22.05.13.
+//  Copyright (c) 2013 Frank Mathy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ETAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ETAppDelegate class]));
+    }
+}
